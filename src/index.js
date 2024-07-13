9c64ec4d45d6d7267ed688f842920bde55f613c5
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-07-13 23:28:55 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import { setupServer } from './server.js';
-setupServer();
+// import { setupServer } from './server.js';
+// setupServer();
+// import { initMongoConnection } from './db/initMongoConnection.js';
+
+// const bootstrap = async () => {
+//   await initMongoConnection();
+//   await setupServer();
+
+
+// };
+
+// bootstrap();
 import { initMongoConnection } from './db/initMongoConnection.js';
+import { setupServer } from './server.js';
 
 const bootstrap = async () => {
-  await initMongoConnection();
-  await setupServer();
-
-
+  try {
+    await initMongoConnection();
+    console.log('Mongo connection successfully established!');
+    await setupServer(); // Запуск сервера після успішного з'єднання
+  } catch (error) {
+    console.error('Error during bootstrap:', error);
+  }
 };
 
-bootstrap();
+bootstrap(); 
